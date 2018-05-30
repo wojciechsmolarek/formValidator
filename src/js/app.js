278e4f3ce1d6f0d419c8049a79c1b9ad2c865ec6
Wojciech Smolarek <kontakt@wojciechsmolarek.pl>
2018-05-30 12:19:11 +0200

    worked script, ready to optymalized

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,31 @@
 (function() {
-  
+
+    function displayErrors(errors, form) { 
+
+        var errorMessage = document.querySelector('.errors');
+
+        if(!errorMessage) {
+            errorMessage = document.createElement('p');
+        }
+
+        errorMessage.className = "errors";
+        errorMessage.innerHTML = errors.join('<br>');
+
+        document.body.appendChild(errorMessage);
+
+    }
+
+    
+
+
 
     var form = document.querySelector("#form"),
-        nameField = form.querySelector("input[name='name]'"),
-        emailField = form.querySelector("input[name='email]'"),
-        textareaField = form.querySelector("textarea[name='textarea]'");
+        nameField = form.querySelector('input[name="name"]'),
+        emailField = form.querySelector('input[name="email"]'),
+        textareaField = form.querySelector('textarea');
 
 
-    console.log(nameField)
+
 
     form.addEventListener("submit", function(e) {
 
@@ -16,9 +34,34 @@
         var errors = [];
 
         if(nameField.value === '') {
-            errors.push("Fill you name");
-            nameField.classList.add('error');
+            errors.push("Fill your name!");
+            nameField.classList.add("error");
+        } else {
+            nameField.classList.remove("error");
         }
+
+        if(emailField.value.indexOf('@') === -1) {
+            errors.push("Incorrect email!");
+            emailField.classList.add("error");
+        } else {
+            emailField.classList.remove("error");
+        }
+
+        if(textareaField.value === '') {
+            errors.push("Enter your message!");
+            textareaField.style.border = '3px solid #ec1212';
+        } else {
+            textareaField.style.border = '';
+        }
+
+
+        if(errors.length) {
+            displayErrors(errors);
+        } else {
+            form.submit();
+        }
+
+
 
     }, false)
 
